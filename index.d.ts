7c8590aa55d883cd7913f40dcd20afb3b5bdcc28
PonyCui <cuis@vip.qq.com>
2017-10-18 18:28:27 +0800

    update d.ts

--- index.d.ts
+++ index.d.ts
@@ -14,6 +14,7 @@ export class Player {
 
     loops: number;
     clearsAfterStop: boolean;
+    fillMode: "Forward" | "Backward"
 
     constructor(canvas: string | HTMLCanvasElement | HTMLDivElement)
     setVideoItem(videoItem: VideoEntity): void
