fa3fc6a9aba4ab20a8a9b4c3b63d2322d7ece9bc
errnull <errnull@163.com>
2017-11-10 15:33:59 +0800

    Correct the illegal radius(roundness)

--- src/Canvas/renderer.js
+++ src/Canvas/renderer.js
@@ -373,6 +373,9 @@ export class Renderer {
         let height = obj._height;
         let radius = obj._cornerRadius;
 
+        if (width < 2 * radius) {radius = width / 2;}
+        if (height < 2 * radius){ radius = height / 2;}
+
         ctx.beginPath();
         ctx.moveTo(x + radius, y);
         ctx.arcTo(x + width, y, x + width, y + height, radius);
