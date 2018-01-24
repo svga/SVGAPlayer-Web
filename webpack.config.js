25a5f69c64e0ca3f421a2a855fe091966df422ba
errnull <errnull@163.com>
2018-01-24 11:28:00 +0800

    change js to ts

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,29 @@ var webpack = require('webpack')
 
 module.exports = {
     entry: {
-        "svga.min": "./src/Canvas/index.js",
-        "svga.createjs.min": "./src/CreateJS/index.js",
-        "svga.layabox.min": "./src/LayaBox/index.js",
+        "svga.min": "./src/Canvas/index.ts",
+        "svga.createjs.min": "./src/CreateJS/index.ts",
+        "svga.layabox.min": "./src/LayaBox/index.ts",
     },
+
     output: {
         path: __dirname,
         filename: "build/[name].js",
         libraryTarget: 'umd',
         library: 'SVGA',
     },
+    
     module: {
-        loaders: [
-            {
-                test: path.join(__dirname, 'src'),
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', "stage-0"]
-                }
-            }
-        ],
+        loaders: [{  
+            test: /\.ts$/,  
+            loader:'ts-loader'  
+        }]
     },
+
+    resolve: {  
+        extensions: ['*', '.webpack.js', '.web.js', '.ts', '.js']  
+    },
+
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
             include: /\.min\.js$/,
