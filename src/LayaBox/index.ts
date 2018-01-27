86065348231f6340a78947d8d1213d69df43585e
errnull <errnull@163.com>
2018-01-27 16:24:33 +0800

    Restart Project

--- src/LayaBox/index.ts
+++
@@ -1,8 +0,0 @@
-import { Parser } from '../parser'
-import { Player } from './player'
-
-module.exports = {
-    Parser,
-    Player,
-    LayaboxPlayer: Player,
-}
---
+++ src/base/parser.ts
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------------------------------------------------
+ *  YYUED Open Source. 
+ *  Licensed under the Apache2.0 License. See License.txt in the project root for license information.
+ *----------------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+export class Parser {
+
+    public load(url: any, success:(result: any) => void, failure:(error: any) => void) {}
+}
