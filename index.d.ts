454f9e7044784f1e3523dcf874a53d82fda42d61
zhaofinger <zhaohaibo1994@gmail.com>
2019-06-11 18:36:38 +0800

    [fix] onPercentage 参数

--- index.d.ts
+++ index.d.ts
@@ -48,7 +48,7 @@ export class Player {
     clearDynamicObjects(): void
     onFinished(callback: () => void): void
     onFrame(callback: () => void): void
-    onPercentage(callback: () => void): void
+    onPercentage(callback: (percentage: number) => void): void
     drawOnContext(ctx: CanvasRenderingContext2D, x: number, y: number, width?: number, height?: number): void
 
 }
